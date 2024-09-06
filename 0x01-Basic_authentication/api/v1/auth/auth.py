5d4824afb18fc15a95f29f0fbb7e78a86e0099be
SammyObeezy <barasam07@gmail.com>
2024-09-06 16:52:00 +0000

    Some Bugs

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
-"""doc doc doc """
-from typing import List, TypeVar
+"""Authentication module.
+"""
 from flask import request
+from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
-    """doc doc doc"""
-
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """doc doc doc"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        """ Method to check if auth is required.
+        """
+        if path is None:
             return True
-        if path[-1] != "/":
-            path += "/"
 
-        return path not in excluded_paths
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """doc doc doc"""
-        if request is None:
-            return None
-        return request.headers.get("Authorization", None)
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
 
-    def current_user(self, request=None) -> TypeVar("User"):
-        """doc doc doc"""
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get user from request.
+        """
         return None
